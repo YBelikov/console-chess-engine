4faefca8195c7da3eca53838e3bfb8055c500647
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-04-23 21:09:06 +0300

    Create regex validation for move command

--- chess/chess/Game.cpp
+++ chess/chess/Game.cpp
@@ -1,11 +1,12 @@
 #include "Game.h"
 #include <iostream>
 #include <string>
+#include <regex>
 
 using std::string;
 using std::cin;
 using std::cout;
-
+using std::regex;
 
 Game::Game() {
 	board = std::make_unique<Board>(8);
@@ -17,12 +18,27 @@ void Game::initializeBoard() {
 
 void Game::start() {
 	initializeBoard();
+	const std::regex moveRegex("[a-h][1-8]-[a-h][1-8]");
 	string command{};
+
 	do {
 		displayer.display(*board, cout);
 		cin >> command;
 		for (auto& c : command) {
 			std::tolower(c);
+		}
+		if (std::regex_match(command, moveRegex)) {
+			
+			Position from;
+			Position to;
+			from.changeXPosition(command[0] - 'a');
+			from.changeYPosition(command[1]);
+			to.changeXPosition(command[3] - 'a');
+			to.changeYPosition(command[4]);
+
+		}
+		if (command == "refresh") { board = std::make_unique<Board>(8);
+			board->intitalize(); 
 		}
 	} while (command != "quit");
 	
