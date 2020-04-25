209b471e9bd7262c99ae6a66f3ff15c65db55d81
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-04-25 19:33:19 +0300

    Create inequality operator for positions

--- chess/chess/Position.cpp
+++ chess/chess/Position.cpp
@@ -9,3 +9,11 @@ int Position::getYPosition() const { return yPos; }
 void Position::changeXPosition(int xOffset) { xPos += xOffset; }
 
 void Position::changeYPosition(int yOffset) { yPos += yOffset; }
+
+bool Position::operator==(const Position& other) {
+	return xPos == other.getXPosition() && yPos == other.getYPosition();
+}
+
+bool Position::operator!=(const Position& other) {
+	return !(*this == other);
+}
