6d9c0b4fc091601ce9c3cbb463cdc0323c4a422c
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-04-26 18:56:01 +0300

    Remove isKilled method from piece class

--- chess/chess/Piece.h
+++ chess/chess/Piece.h
@@ -10,13 +10,10 @@ class Piece {
 protected:
 	PieceType type;
 	Color color;
-	bool killed;
 public:
 	Piece(PieceType type_, Color color_, bool killed_ = false) : type(type_), color(color_), killed(killed_) {}
 	PieceType getPieceType() const noexcept { return type; }
 	Color getColorOfPiece() const noexcept { return color; }
-	bool isKilled() const noexcept { return killed; }
-	void setKilled() noexcept { killed = true; }
 	virtual void canMove(Game& , const Position&, const Position&) {};
 	virtual ~Piece() {}
 	
